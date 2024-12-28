94fe957aa2fd1824538b024784e0f0eaba6aa440
SoloDevAbu <abubakkar2502@gmail.com>
2024-12-28 23:26:40 +0530

    Modify vite config for routing in vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: './index.html',
+    },
+  },
+  server: {
+    open: true,
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+});
